8a7a30a3b1be5fe20a583ce4ddf827d1a4b18ba4
Madone <ahamad.jedu@gmail.com>
2016-07-29 20:06:56 +0700

    ฟังค์อับโหลดรูป

--- lib/function.php
+++ lib/function.php
@@ -1,16 +1,19 @@
 <?php
 
-
-function convDateToDb($strDate){
-    $year = '0000'; $month = '00'; $day = '00';
-    list($day,$month,$year) = explode('-', $strDate);
-    return @implode('-',[$year,$month,$day]);    
+function convDateToDb($strDate) {
+    $year = '0000';
+    $month = '00';
+    $day = '00';
+    list($day, $month, $year) = explode('-', $strDate);
+    return @implode('-', [$year, $month, $day]);
 }
 
-function convDateToDisplay($strDate){
-    $year = '0000'; $month = '00'; $day = '00';
-    list($year,$month,$day) = explode('-', $strDate);
-    return @implode('-',[$day,$month,$year]);    
+function convDateToDisplay($strDate) {
+    $year = '0000';
+    $month = '00';
+    $day = '00';
+    list($year, $month, $day) = explode('-', $strDate);
+    return @implode('-', [$day, $month, $year]);
 }
 
 /**
@@ -28,7 +31,7 @@ function DateThai($strDate) {
     $strSeconds = date("s", strtotime($strDate));
     $strMonthCut = Array("", "ม.ค.", "ก.พ.", "มี.ค.", "เม.ย.", "พ.ค.", "มิ.ย.", "ก.ค.", "ส.ค.", "ก.ย.", "ต.ค.", "พ.ย.", "ธ.ค.");
     $strMonthThai = $strMonthCut[$strMonth];
-    return $strDay . " " . $strMonthThai . " " . $strYear ;
+    return $strDay . " " . $strMonthThai . " " . $strYear;
 }
 
 /**
@@ -58,27 +61,27 @@ function DateTimeThai($strDate) {
  */
 function pagination($size, $count) {
     global $_GET;
-    
+
     $page['p'] = isset($_GET['p']) ? $_GET['p'] : 1;
-    $page['start'] = ($page['p']==1) ? 0 : ($size*$page['p'])-$size;
-    $page['size'] = $size>$count?$count:$size;
-    $num = (int)($count / $size);
-    $num = ($count % $size) ? $num+1 : $num;
-    
-    $page['widget'] = '<div class="row"><div class="col-sm-12">';   
-    $page['widget'] .= 'แสดง '.($page['start']+1).' ถึง '.($page['size']*$page['p']).' จาก '.$count; 
-    
+    $page['start'] = ($page['p'] == 1) ? 0 : ($size * $page['p']) - $size;
+    $page['size'] = $size > $count ? $count : $size;
+    $num = (int) ($count / $size);
+    $num = ($count % $size) ? $num + 1 : $num;
+
+    $page['widget'] = '<div class="row"><div class="col-sm-12">';
+    $page['widget'] .= 'แสดง ' . ($page['start'] + 1) . ' ถึง ' . ($page['size'] * $page['p']) . ' จาก ' . $count;
+
     $page['widget'] .= '<div class="dataTables_paginate paging_simple_numbers" id="dataTables-example_paginate">';
     $page['widget'] .='<ul class="pagination">';
-    $previous=(($page['p']-1)>0)?($page['p']-1):'';
-    $page['widget'] .='<li class="paginate_button previous '.($previous==''?'disabled':'').'" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_previous"><a href="' . ($previous?genGet(['p'=>$previous]):'#') . '">Previous</a></li>';
+    $previous = (($page['p'] - 1) > 0) ? ($page['p'] - 1) : '';
+    $page['widget'] .='<li class="paginate_button previous ' . ($previous == '' ? 'disabled' : '') . '" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_previous"><a href="' . ($previous ? genGet(['p' => $previous]) : '#') . '">Previous</a></li>';
 
     for ($i = 1; $i <= $num; $i++) {
-        $active = ($i==$page['p'])?'active':'';
-        $page['widget'] .='<li class="paginate_button '.$active.'" aria-controls="dataTables-example" tabindex="0"><a href="' . genGet(['p'=>$i]) . '">' . $i . '</a></li>';
+        $active = ($i == $page['p']) ? 'active' : '';
+        $page['widget'] .='<li class="paginate_button ' . $active . '" aria-controls="dataTables-example" tabindex="0"><a href="' . genGet(['p' => $i]) . '">' . $i . '</a></li>';
     }
-    $next = (($page['p']+1)<=$num)?($page['p']+1):'';
-    $page['widget'] .='<li class="paginate_button next '.($next==''?'disabled':'').'" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_next"><a href="' . ($next?genGet(['p'=>$next]):'#') . '">Next</a></li>';
+    $next = (($page['p'] + 1) <= $num) ? ($page['p'] + 1) : '';
+    $page['widget'] .='<li class="paginate_button next ' . ($next == '' ? 'disabled' : '') . '" aria-controls="dataTables-example" tabindex="0" id="dataTables-example_next"><a href="' . ($next ? genGet(['p' => $next]) : '#') . '">Next</a></li>';
     $page['widget'] .='</ul></div></div></div>';
     return $page;
 }
@@ -89,10 +92,10 @@ function pagination($size, $count) {
  * @param type $add
  * @return type
  */
-function genGet($add=[]) {
+function genGet($add = []) {
     global $_GET;
     $param = [];
-    $_GET = $add?  array_merge($_GET,$add):$_GET;
+    $_GET = $add ? array_merge($_GET, $add) : $_GET;
     foreach ($_GET as $key => $val) {
         $param[] = $key . "=" . $val;
     }
@@ -111,11 +114,11 @@ function genGetHidden() {
     $param = [];
     //$_GET = $add?  array_merge($_GET,$add):$_GET;
     foreach ($_GET as $key => $val) {
-        $param[$key] = "<input type='hidden' name='".$key ."' value='". $val."' />";
+        $param[$key] = "<input type='hidden' name='" . $key . "' value='" . $val . "' />";
     }
     //print_r($param);
     //$param = $add?  array_merge($param,$add):[];
-    return $param ?  @implode('', $param) : '';
+    return $param ? @implode('', $param) : '';
 }
 
 /**
@@ -124,20 +127,64 @@ function genGetHidden() {
  * @param type $id
  * @return type
  */
-function findPrivilege($id){
+function findPrivilege($id) {
     global $db;
-    $privilege = $db->select("privilege")->where(['pv_id = '.$id])->one();
-    return $privilege->pv_id==$id?$privilege->pv_name:'ไม่พบข้อมูล';
-
+    $privilege = $db->select("privilege")->where(['pv_id = ' . $id])->one();
+    return $privilege->pv_id == $id ? $privilege->pv_name : 'ไม่พบข้อมูล';
 }
 
-function genSrcImg($fileName,$sub = 'patient/'){
-    $file = 'uploads/'.$sub.$fileName.'.jpg';
+function genSrcImg($fileName, $sub = 'patient/') {
+    $file = 'uploads/' . $sub . $fileName . '.jpg';
     $fileStaby = 'uploads/person.jpg';
-    if(file_exists($file)){
+    if (file_exists($file)) {
         return $file;
-    }else{
+    } else {
         return $fileStaby;
     }
- 
+}
+
+function uploadFile($file, $subPath = '',$newNamFile='') {
+    $target_dir = _pathUpload . ($subPath?$subPath.'/':'');
+    $target_file = $target_dir . basename($newNamFile.'.jpg');
+    $uploadOk = 1;
+    //$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+//    if (isset($file)) {
+//        $check = getimagesize($file["tmp_name"]);
+//        if ($check !== false) {
+//            echo "File is an image - " . $check["mime"] . ".";
+//            $uploadOk = 1;
+//        } else {
+//            echo "File is not an image.";
+//            $uploadOk = 0;
+//        }
+//    }
+// Check if file already exists
+//    if (file_exists($target_file)) {
+//        echo "Sorry, file already exists.";
+//        $uploadOk = 0;
+//    }
+// Check file size
+    if ($file["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+// Allow certain file formats
+//    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+//        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//        $uploadOk = 0;
+//    }
+// Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($file["tmp_name"], $target_file)) {
+            //echo "The file " . basename($file["name"]) . " has been uploaded.";
+            return $file["tmp_name"];
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+           // exit();
+        }
+    }
 }
