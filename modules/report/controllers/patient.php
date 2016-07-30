251bf6f69c105cae98c1a5b40c3ed61e7e0a3af8
Madone <ahamad.jedu@gmail.com>
2016-07-30 16:16:31 +0700

    แก้ไขปัญหา "ไม่แสดงข้อมูลสิทธิการรักษา "

--- modules/report/controllers/patient.php
+++ modules/report/controllers/patient.php
@@ -3,19 +3,19 @@
 $title = 'รายงานผู้ป่วย';
 $t_date_start = date("d-m-Y");
 $t_date_end = date("d-m-Y");
-if(isset($_POST['search'])){
-$data = $db->sql("SELECT
+if (isset($_POST['search'])) {
+    $data = $db->sql("SELECT
 treat.*,
 patient.p_name,
 patient.p_surname
 FROM
 treat
 INNER JOIN patient ON patient.p_id = treat.p_id ")->where([
-    "DATE(treat.t_date) >= '".convDateToDb($_POST['t_date_start'])."'",
-    "DATE(treat.t_date) <= '".convDateToDb($_POST['t_date_end'])."'",
-        ])->all();
+                "DATE(treat.t_date) >= '" . convDateToDb($_POST['t_date_start']) . "'",
+                "DATE(treat.t_date) <= '" . convDateToDb($_POST['t_date_end']) . "'",
+            ])->all();
 //echo $db->sql;
-$t_date_start =$_POST['t_date_start'];
-$t_date_end =$_POST['t_date_end'];
+    $t_date_start = $_POST['t_date_start'];
+    $t_date_end = $_POST['t_date_end'];
 }
 
--- modules/report/views/patient.php
+++ modules/report/views/patient.php
@@ -23,46 +23,41 @@
 
 <div class="panel panel-default">
     <div class="panel-heading hidden-print">
-      &nbsp;
+        &nbsp;
         <div class="pull-right">
-            <a onclick="window.print(); return false;" class="btn btn-default btn-xs"><i class="fa fa-print"></i></a>
+            <a onclick="window.print();
+                    return false;" class="btn btn-default btn-xs"><i class="fa fa-print"></i></a>
         </div>
     </div>
     <div class="panel-body">
-            
-            <h2> <?= $title ?></h2>
-            <p>
-            <?= DateThai($t_date_start) ?> ถึง <?= DateThai($t_date_end) ?>
-            </p>
-       
+
+        <h2> <?= $title ?></h2>
+        <p>
+<?= DateThai($t_date_start) ?> ถึง <?= DateThai($t_date_end) ?>
+        </p>
+
         <table class="table table-striped table-bordered table-hover dataTable no-footer">
             <tr>
                 <td>รหัสผู้ป่วย</td>
                 <td>ชื่อ-สกุล</td>
-                <td>สิทธิการรักษา</td>
+                <td>วินิจัยโรค</td>
                 <td>เมื่อ</td>
 
             </tr>
 
             <?php
             foreach ($data as $val) {
-                ?>
-                <tr>
-                    <td>
-                        <?= $val->p_id ?>
-                    </td>
-                    <td>
-                        <?= $val->p_name . " " . $val->p_surname; ?>
-                    </td>
-                    <td>
-                        <?= $val->resultjude ?>
-                    </td>
-                    <td>
-                        <?= DateTimeThai($val->t_date) ?>
-                    </td>
-
-                </tr>
-            <?php } ?>
+                if ($val->resultjude) {
+                    ?>
+                    <tr>
+                        <td><?= $val->p_id ?></td>
+                        <td><?= $val->p_name . " " . $val->p_surname; ?></td>
+                        <td><?= $val->resultjude ?></td>
+                        <td><?= DateTimeThai($val->t_date) ?></td>
+                    </tr>
+                <?php }
+            }
+            ?>
         </table>
     </div>
 </div>
@@ -74,11 +69,11 @@
 <script type="text/javascript">
                 $(function () {
                     $('#t_date_start').datepicker({
-                        autoclose:true,
+                        autoclose: true,
                         format: 'dd-mm-yyyy'
                     });
                     $('#t_date_end').datepicker({
-                        autoclose:true,
+                        autoclose: true,
                         format: 'dd-mm-yyyy'
                     });
                 });
